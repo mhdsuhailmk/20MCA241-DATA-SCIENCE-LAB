75cb44d68f7c68b33153c9f6efb01470e19880ba
MUHAMMED SUHAIL MK <mksuhail787@gmail.com>
2022-10-26 09:54:46 +0530

    Update 4-knn.py

--- EXERCISE-4-KNN/4-knn.py
+++ EXERCISE-4-KNN/4-knn.py
@@ -4,25 +4,25 @@
 # In[15]:
 
 
-#importing dataset(remote)
+#DATASET IMPORTING...
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 
-#loading dataset
+#DATASET LOADING...
 iris = load_iris()
 x = iris.data
 y = iris.target
 
-#splitting the dataset into test ,train
+#SPLITTING THE DATASET
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3,random_state=1)
 c_knn = KNeighborsClassifier(n_neighbors=5)
 c_knn.fit(x_train,y_train)
 y_pred = c_knn.predict(x_test)
 
 
-#to check accuracy of the model
+#CHECK ACCURACY...
 print("Accuracy : ",metrics.accuracy_score(y_test,y_pred))
 
 sample = [[2,2,7,3]]
