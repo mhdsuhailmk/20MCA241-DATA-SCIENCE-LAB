825611633096a8ab36958fd111966f9352f9e81f
MUHAMMED SUHAIL MK <mksuhail787@gmail.com>
2022-11-02 09:30:46 +0530

    Update 5naive.py

--- EXERCISE-5-NAIVEBAYES/5naive.py
+++ EXERCISE-5-NAIVEBAYES/5naive.py
@@ -2,12 +2,12 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
-X,y=load_iris(return_X_y=True)
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.5,random_state=0)
+x,y=load_iris(return_x_y=True)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.4,random_state=0)
 gnb=GaussianNB()
-y_pred=gnb.fit(X_train,y_train).predict(X_test)
+y_pred=gnb.fit(x_train,y_train).predict(x_test)
 print(y_pred)
 x_new=[[4,4,3,3]]
-y_new=gnb.fit(X_train,y_train).predict(x_new)
+y_new=gnb.fit(x_train,y_train).predict(x_new)
 print("predicted output for [[4,4,3,3]]:",y_new)
-print("Naive bayes score               :",gnb.score(X_test,y_test))
+print("Naive bayes score               :",gnb.score(x_test,y_test))
